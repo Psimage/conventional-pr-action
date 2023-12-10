6e3867c22a075b35044ddfc4fada9144831246a0
Yaroslav Buhaiev <ybuhaiev@playtika.com>
2023-12-10 22:26:07 +0200

    5

--- src/main.ts
+++ src/main.ts
@@ -7,26 +7,12 @@ import * as github from '@actions/github'
  */
 export async function run(): Promise<void> {
   try {
-    await doRun()
+    await getPrTitle()
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 
-}
-
-async function doRun() {
-  const ms: string = core.getInput('milliseconds')
-
-  // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-  core.debug(`Waiting ${ms} milliseconds ...`)
-
-  // Log the current timestamp, wait, then log the new timestamp
-  core.debug(new Date().toTimeString())
-  core.debug(new Date().toTimeString())
-
-  // Set outputs for other workflow steps to use
-  core.setOutput('time', new Date().toTimeString())
 }
 
 async function getPrTitle(): Promise<string> {
@@ -40,11 +26,11 @@ async function getPrTitle(): Promise<string> {
   core.info('PR title from context: ' + github.context.payload.pull_request.title)
 
   const client = github.getOctokit(core.getInput('github-token'))
-  const owner = prContext.base.user.login;
-  const repo = prContext.base.repo.name;
-  const prTitleFromAPI = (await client.rest.pulls.get({owner, repo, pull_number: prContext.number})).data.title
+  const owner = prContext.base.user.login
+  const repo = prContext.base.repo.name
+  const prTitleFromAPI = (await client.rest.pulls.get({ owner, repo, pull_number: prContext.number })).data.title
 
   core.info('PR title from API: ' + prTitleFromAPI)
 
-  return ""
+  return ''
 }
